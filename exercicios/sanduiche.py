1d556b030dad92977f35d90494bd9015391eed1e
EtCiano <miguelfmellobirck@gmail.com>
2026-08-11 21:09:45 -0300

    sanduíche, de maneira meio limitada

--- exercicios/sanduiche.py
+++ exercicios/sanduiche.py
@@ -1,4 +1,5 @@
 # Sanduíche (sanduiche.py)
+
 primeiraLinha = [int(x) for x in input().split()]
 quantidadeIngredientes = primeiraLinha[0]
 quantidadeIncompativeis = primeiraLinha[1]
@@ -6,22 +7,40 @@ quantidadeIncompativeis = primeiraLinha[1]
 incompativeis = []
 
 for i in range(quantidadeIncompativeis):
-  incompativeis.append(input().replace(" ", ""))
+    incompativeis.append(input().split())
 
 combinacoes = []
 
-for i in range(quantidadeIngredientes+1):
-  for j in range(quantidadeIngredientes+1):
-    combinacao = str(i) + str(j)
-    # não funciona com combinações de 3 ingredientes ou mais
-    # não sei se tem combinação de mais de 3 ingredientes
-    if i == j:
-      continue
-    if combinacao in incompativeis or combinacao[::-1] in incompativeis:
-      continue
-    if combinacao in combinacoes or combinacao[::-1] in combinacoes:
-      continue
+def checarBase(numero, base):
+    numeroAbs = str(abs(int(numero)))
+    numerosBase = "".join([str(x) for x in range(base+1)])
+    return all(c in numerosBase for c in numeroAbs)
 
-    combinacoes.append(combinacao)
+numeroMaximo = int('9' * quantidadeIngredientes)
 
-print(len(combinacoes))
+for i in range(numeroMaximo + 1):
+    if checarBase(i, quantidadeIngredientes) and '0' not in str(i):
+        combinacoes.append(list(str(i)))
+
+def combinacaoValida(combinacao):
+    for digito in combinacao:
+        if combinacao.count(digito) > 1:
+            return False
+    for incompativel in incompativeis:
+        if set(incompativel).issubset(combinacao):
+            return False
+    return True
+
+combinacoesVistas = set()
+novaCombinacoes = []
+
+for combinacao in combinacoes:
+    if not combinacaoValida(combinacao):
+        continue
+    assinatura = tuple(sorted(combinacao))
+    if assinatura in combinacoesVistas:
+        continue
+    combinacoesVistas.add(assinatura)
+    novaCombinacoes.append(combinacao)
+
+print(len(novaCombinacoes))
